386972c621c309eda1a159d302e521a6d122664b
Karthikeya Mekala <mail2mkarthik@gmail.com>
2022-12-28 10:48:08 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,6 @@ if streamlit.button("Get Fruit Load List"):
     my_data_row = get_fruit_load_list()
     streamlit.dataframe(my_data_row)
         
-streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button("Add a Fruit to the List"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
